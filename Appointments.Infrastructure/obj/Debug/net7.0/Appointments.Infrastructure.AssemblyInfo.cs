00794cf4bcaa70fde16a5253ffa9a8ec7c933025
CauaBelodi <cauacardosoyt@gmail.com>
2024-01-26 11:47:59 -0300

    All Done

--- Appointments.Infrastructure/obj/Debug/net7.0/Appointments.Infrastructure.AssemblyInfo.cs
+++ Appointments.Infrastructure/obj/Debug/net7.0/Appointments.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Appointments.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b349989ace9e38d93a15004564167ce7267eca1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a9b319e70a66fbed14aa4adb86acb3e3807fc1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Appointments.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Appointments.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
